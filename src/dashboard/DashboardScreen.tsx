d95ff43074195dfba5a47b299f749e7763dc3ccb
Alex <zhmurik@yandex.ru>
2020-10-02 15:13:31 -0400

    dashbord mock

--- src/dashboard/DashboardScreen.tsx
+++ src/dashboard/DashboardScreen.tsx
@@ -1,20 +1,44 @@
+import { Box } from 'grommet';
 import React from 'react';
-import logo from './logo.svg';
-import styles from './DashboardScreen.module.scss';
+import { PieChart } from './PieChart';
 
+const clients = {
+  users: 526,
+  customers: 35,
+};
+
+const purchaseCount = [clients.users - clients.customers, 23, 9, 3];
 export const Dashboard = () => (
-  <>
-    <img src={logo} className={styles.appLogo} alt="logo" />
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload.
-    </p>
-    <a
-      className={styles.appLink}
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </>
+  <Box
+    align="center"
+    justify="center"
+    direction="row"
+    gap="medium"
+    height="150px"
+  >
+    <PieChart
+      customColor
+      data={[
+        {
+          id: 'cust',
+          label: 'paying customers',
+          value: clients.customers,
+          color: '#60CDBA',
+        },
+        {
+          id: 'all',
+          label: 'never bought',
+          value: clients.users - clients.customers,
+          color: 'rgb(233, 193, 159)',
+        },
+      ]}
+    />
+    <PieChart
+      data={purchaseCount.map((count, index) => ({
+        id: index,
+        label: `${index} payments`,
+        value: count,
+      }))}
+    />
+  </Box>
 );
