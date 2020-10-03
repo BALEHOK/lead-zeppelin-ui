1a3d45a14d9f46a29dcb636afbe141137d32a6ab
Alex <zhmurik@yandex.ru>
2020-10-02 23:06:12 -0400

    removing relay

--- relay.config.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const { schemaPath } = JSON.parse(fs.readFileSync('.graphqlconfig', 'utf-8'));
-
-module.exports = {
-  src: './src',
-  schema: schemaPath,
-  language: 'typescript',
-  exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
-};
